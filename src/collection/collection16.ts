5622a1819ae9f9337a21dfa20276378e42982c63
DaniilGladskikh <slysaa@mail.ru>
2026-05-05 15:38:28 +0500

    Update collection16.ts
    Добавлена функция squaresWithIndex для маппинга чисел в объекты

--- src/collection/collection16.ts
+++ src/collection/collection16.ts
@@ -5,5 +5,10 @@
 export type Result = {num: number, square: number, index: number};
 
 export function squaresWithIndex(numbers: number[]): Result[] {
-
+	// Используем map для создания объектов с числом, его квадратом и индексом
+	return numbers.map((num, index) => ({
+		num,
+		square: num * num,
+		index
+	}));
 }
