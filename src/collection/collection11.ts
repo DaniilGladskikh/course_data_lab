dcd3445f2fd03292c77458bb9dd3507c7bc35053
DaniilGladskikh <slysaa@mail.ru>
2026-05-05 14:16:36 +0500

    Update collection11.ts
    Добавлена реализация функции groupBy для группировки объектов с помощью Map

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -3,5 +3,14 @@
 */
 
 export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
-
+	const map = new Map<T[keyof T], T[]>();
+	// Группируем объекты по заданному ключу
+	for (const item of arr) {
+		const keyValue = item[key];
+		if (!map.has(keyValue)) {
+			map.set(keyValue, []);
+		}
+		map.get(keyValue)!.push(item);
+	}
+	return map;
 }
