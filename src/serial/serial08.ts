049b6bc78634a589c4e8499940ebc7ffb05a611e
DaniilGladskikh <slysaa@mail.ru>
2026-05-05 20:41:43 +0500

    Update serial08.ts
    Реализована полиморфная десериализация фигур с использованием декоратора @Type и дискриминатора

--- src/serial/serial08.ts
+++ src/serial/serial08.ts
@@ -2,11 +2,15 @@
 	Обеспечьте сериализацию и десериализацию объектов классов Circle, Rectangle и Drawing
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
+
 export abstract class Shape {
   abstract area(): number;
 }
 
 export class Circle extends Shape {
+  type: string = 'circle';
   radius: number;
   constructor(radius: number) {
     super();
@@ -18,6 +22,7 @@ export class Circle extends Shape {
 }
 
 export class Rectangle extends Shape {
+  type: string = 'rectangle';
   width: number;
   height: number;
   constructor(width: number, height: number) {
@@ -33,6 +38,16 @@ export class Rectangle extends Shape {
 export class Drawing {
   name: string;
 
+  @Type(() => Shape, {
+    keepDiscriminatorProperty: true,
+    discriminator: {
+      property: 'type',
+      subTypes: [
+        { value: Circle, name: 'circle' },
+        { value: Rectangle, name: 'rectangle' },
+      ],
+    },
+  })
   shapes: Shape[];
   constructor(name: string, shapes: Shape[]) {
     this.name = name;
