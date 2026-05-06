dd4de3602ae2308df8531d27ade62170641885ae
DaniilGladskikh <slysaa@mail.ru>
2026-05-06 19:18:41 +0500

    Update mongo19.ts
    Реализован расчет суммы заказов со скидкой через агрегацию в коллекции orders

--- src/mongo/mongo19.ts
+++ src/mongo/mongo19.ts
@@ -23,9 +23,23 @@ export interface OrderTotal {
 }
 
 export async function get_order_totals(db: Db): Promise<OrderTotal[]> {
-	// TODO: Рассчитать итоговую сумму для каждого заказа с учетом скидки
+	// Рассчитать итоговую сумму для каждого заказа с учетом скидки
 	// Поля: product, total (quantity * unitPrice), finalTotal (total - discount)
 	return await db.collection("orders").aggregate([
-
-	]).toArray() as OrderTotal[]
+		{
+			$project: {
+				_id: 0,
+				product: 1,
+				quantity: 1,
+				unitPrice: 1,
+				discount: 1,
+				total: { $multiply: ["$quantity", "$unitPrice"] }
+			}
+		},
+		{
+			$addFields: {
+				finalTotal: { $subtract: ["$total", "$discount"] }
+			}
+		}
+	]).toArray() as unknown as OrderTotal[]
 }
