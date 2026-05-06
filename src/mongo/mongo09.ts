866b4fe24c30d4c8e9b1cc66136af2bc6e22d40f
DaniilGladskikh <slysaa@mail.ru>
2026-05-06 15:04:51 +0500

    Update mongo09.ts
    Реализован поиск автомобилей по диапазону года выпуска и цены в коллекции cars

--- src/mongo/mongo09.ts
+++ src/mongo/mongo09.ts
@@ -14,6 +14,9 @@ export class Car {
 }
 
 export async function find_cars_in_range(db: Db, minYear: number, maxPrice: number): Promise<Car[]> {
-    // TODO: Найти автомобили с годом выпуска >= minYear и ценой <= maxPrice
-	return db.collection("cars")
+    // Находим автомобили с годом выпуска >= minYear и ценой <= maxPrice
+    return await db.collection("cars").find({
+        year: { $gte: minYear },
+        price: { $lte: maxPrice }
+    }).toArray() as unknown as Car[]
 }
