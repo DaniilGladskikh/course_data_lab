6f90184bbb08067d54ce9d45f3bdb0d437104fb5
DaniilGladskikh <slysaa@mail.ru>
2026-05-06 18:43:07 +0500

    Update mongo17.ts
    Реализован поиск самых продаваемых товаров через агрегацию в коллекции products

--- src/mongo/mongo17.ts
+++ src/mongo/mongo17.ts
@@ -18,9 +18,11 @@ export interface TopProduct {
 }
 
 export async function get_top_selling_products(db: Db, limit: number): Promise<TopProduct[]> {
-    // TODO: Найти топ-N продуктов по количеству продаж
-	return  await db.collection("products").aggregate([
-
-	]).toArray() as TopProduct[]
+    // Находим топ-N продуктов по количеству продаж
+    return await db.collection("products").aggregate([
+        { $sort: { sales: -1 } },
+        { $limit: limit },
+        { $project: { _id: 0, name: 1, sales: 1, price: 1 } }
+    ]).toArray() as unknown as TopProduct[]
 }
 
