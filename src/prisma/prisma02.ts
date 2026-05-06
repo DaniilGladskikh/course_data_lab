0e23f19359a53ee60f112fad51c75499a41b2c9b
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 00:52:10 +0500

    Update prisma02.ts
    Реализован поиск курсов по дате создания через фильтр gt

--- src/prisma/prisma02.ts
+++ src/prisma/prisma02.ts
@@ -2,7 +2,11 @@ import { prisma } from './prisma_init'
 
 export async function find_courses_after_date(date: Date) {
     return await prisma.course.findMany({
-    // TODO: Найти все курсы, созданные после указанной даты
-    // Вернуть массив курсов
+        // Найти все курсы, созданные после указанной даты
+        where: {
+            createdAt: {
+                gt: date
+            }
+        }
     })
 }
