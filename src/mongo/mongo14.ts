326cf6ab09ee3bd46eae61b4dc9cf22917363874
DaniilGladskikh <slysaa@mail.ru>
2026-05-06 18:05:18 +0500

    Update mongo14.ts
    Реализовано удаление участника команды из проекта в коллекции projects

--- src/mongo/mongo14.ts
+++ src/mongo/mongo14.ts
@@ -12,6 +12,9 @@ export class Project {
 }
 
 export async function remove_member_from_project(db: Db, projectName: string, memberToRemove: string) {
-	// TODO: Удалить указанного участника из команды проекта
-	db.collection("projects")
+	// Удаляем указанного участника из команды проекта
+	await db.collection("projects").updateOne(
+		{ name: projectName },
+		{ $pull: { team: memberToRemove } }
+	)
 }
