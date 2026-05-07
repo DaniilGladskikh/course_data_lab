9cc346001afb262aef4d2ed2db779e81f1d76a1e
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 20:00:49 +0500

    Update prisma29.ts
    Реализован поиск студентов с одинаковыми именами через группировку

--- src/prisma/prisma29.ts
+++ src/prisma/prisma29.ts
@@ -1,7 +1,25 @@
 import { prisma } from './prisma_init'
 
 export async function find_students_with_same_names() {
-    // TODO: Найти студентов с одинаковыми именами
+    // Найти студентов с одинаковыми именами
     // Использовать обработку в TypeScript
-    // Вернуть массив групп студентов с одинаковыми именами
+    const students = await prisma.student.findMany({
+        include: {
+            person: true
+        }
+    })
+
+    const groups: { [name: string]: typeof students } = {}
+    for (const student of students) {
+        const name = student.person.name
+        if (!groups[name]) groups[name] = []
+        groups[name].push(student)
+    }
+
+    return Object.entries(groups)
+        .filter(([name, list]) => list.length > 1)
+        .map(([name, list]) => ({
+            name,
+            students: list
+        }))
 }
