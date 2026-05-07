6adc04996ec5642c0e08611b6b9d2bfb3c9fa1fb
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 15:57:38 +0500

    Update prisma19.ts
    Реализован поиск самого популярного курса через groupBy

--- src/prisma/prisma19.ts
+++ src/prisma/prisma19.ts
@@ -1,7 +1,31 @@
 import { prisma } from './prisma_init'
 
 export async function find_most_popular_course() {
-    // TODO: Найти курс с максимальным количеством уникальных студентов
+    // Найти курс с максимальным количеством уникальных студентов
     // Использовать агрегацию средствами Prisma (groupBy)
-    // Вернуть объект курса с дополнительным полем studentCount
+    const stats = await prisma.grade.groupBy({
+        by: ['courseId'],
+        _count: {
+            studentId: true
+        },
+        orderBy: {
+            _count: {
+                studentId: 'desc'
+            }
+        },
+        take: 1
+    })
+
+    if (stats.length === 0) return null
+
+    const course = await prisma.course.findUnique({
+        where: { id: stats[0].courseId }
+    })
+
+    if (!course) return null
+
+    return {
+        ...course,
+        studentCount: stats[0]._count.studentId
+    }
 }
