7093d79359e5b03c313db82afd9d22b181f1dc17
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 17:59:11 +0500

    Update prisma23.ts
    Реализовано удаление студентов

--- src/prisma/prisma23.ts
+++ src/prisma/prisma23.ts
@@ -1,7 +1,18 @@
 import { prisma } from './prisma_init'
 
 export async function delete_students_without_person() {
-    // TODO: Удалить студентов, у которых нет связанной записи в person
+    // Удалить студентов, у которых нет связанной записи в person
     // Использовать фильтрацию средствами Prisma
-    // Вернуть количество удаленных записей
+    const result = await prisma.student.deleteMany({
+        where: {
+            NOT: {
+                person: {
+                    id: {
+                        gt: 0
+                    }
+                }
+            }
+        }
+    })
+    return result.count
 }
