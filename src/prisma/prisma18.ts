7b471cdedac18aba1afdb1cddac1ced0abf200df
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 14:50:55 +0500

    Update prisma18.ts
    Реализован поиск отличников через фильтрацию

--- src/prisma/prisma18.ts
+++ src/prisma/prisma18.ts
@@ -1,7 +1,16 @@
 import { prisma } from './prisma_init'
 
 export async function find_students_with_excellent_grades() {
-    // TODO: Найти всех студентов, у которых есть хотя бы одна оценка 5
+    // Найти всех студентов, у которых есть хотя бы одна оценка 5
     // Использовать обработку данных в TypeScript (не фильтрацию Prisma)
-    // Вернуть массив студентов с информацией о person
+    const students = await prisma.student.findMany({
+        include: {
+            person: true,
+            grades: true
+        }
+    })
+
+    return students.filter(student => 
+        student.grades.some(g => g.grade === 5)
+    )
 }
