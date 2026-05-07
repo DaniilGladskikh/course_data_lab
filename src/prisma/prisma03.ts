5de8f65c9167f141ec25c919d2db37fa8ae5b750
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 10:40:56 +0500

    Update prisma03.ts
    Реализован поиск студентов по подстроке имени через contains

--- src/prisma/prisma03.ts
+++ src/prisma/prisma03.ts
@@ -2,7 +2,17 @@ import { prisma } from './prisma_init'
 
 export async function find_students_by_name_pattern(pattern: string) {
     return await prisma.student.findMany({
-    // TODO: Найти всех студентов, у которых имя содержит указанную подстроку
-    // Вернуть массив студентов с включенной информацией о person
+        // Найти всех студентов, у которых имя содержит указанную подстроку
+        where: {
+            person: {
+                name: {
+                    contains: pattern,
+                    mode: 'insensitive'
+                }
+            }
+        },
+        include: {
+            person: true
+        }
     })
 }
