29d173c47986e5148aa288edd2e945d1198f1da0
DaniilGladskikh <slysaa@mail.ru>
2026-05-07 16:17:24 +0500

    Update prisma20.ts
    Реализовано обновление имен студентов по паттерну через цикл и update

--- src/prisma/prisma20.ts
+++ src/prisma/prisma20.ts
@@ -1,7 +1,24 @@
 import { prisma } from './prisma_init'
 
 export async function update_student_names_pattern(oldPattern: string, newPattern: string) {
-    // TODO: Обновить имена студентов, заменяя oldPattern на newPattern
-    // Использовать обработку в TypeScript с отдельными запросами update
-    // Вернуть количество обновленных студентов
+    // Найти всех людей, у которых имя содержит oldPattern
+    const people = await prisma.person.findMany({
+        where: {
+            name: {
+                contains: oldPattern
+            }
+        }
+    })
+
+    let count = 0
+    for (const person of people) {
+        // Заменяем все вхождения паттерна
+        const newName = person.name.split(oldPattern).join(newPattern)
+        await prisma.person.update({
+            where: { id: person.id },
+            data: { name: newName }
+        })
+        count++
+    }
+    return count
 }
